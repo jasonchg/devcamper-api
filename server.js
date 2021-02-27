41197c76b448448c4d2fbf9e31c597cffd341648
Jcpro <jcproductions.my@gmail.com>
2021-02-28 02:41:44 +0800

    Added : Setup serverjs with basic routes

--- server.js
+++ server.js
@@ -3,12 +3,52 @@ const env = require('dotenv')
 
 // Load env vars
 env.config({
-  path: './config/config.eng',
+  path: './config/config.env',
+})
+const app = express()
+const PORT = process.env.PORT || 5000
+
+// Routes
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send(
+      '<div style="display:flex; flex-direction:row; justify-content:center; background:blue; color:white;width:100%; font-size:24px; overflow:none;padding:20px 0;">Api is running</div>'
+    )
 })
 
-const app = express()
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: 'show all bootcamps',
+  })
+})
 
-const PORT = process.env.PORT || 5000
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: 'create all bootcamps',
+  })
+})
+
+app.put('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: 'update all bootcamps',
+  })
+})
+
+app.delete('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: 'delete all bootcamps',
+  })
+})
+
+// Middleware
+
+// Listener
 
 app.listen(
   PORT,
