3d4c492bcb1970083cc892900e23109b141b557d
Jcpro <jcproductions.my@gmail.com>
2021-02-28 11:44:17 +0800

    Added : Bootcamp Model

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
 const express = require('express')
 const env = require('dotenv')
 const morgan = require('morgan')
+const connectDB = require('./config/db.js')
+const colors = require('colors')
 
 // Route files
 const bootcamps = require('./routes/bootcamps.js')
-const logger = require('./middlewares/logger.js')
 
 // Load env vars
 env.config({
   path: './config/config.env',
 })
+
+// Connect to database
+connectDB()
+
 const app = express()
 const PORT = process.env.PORT || 5000
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
-  app.get('/', (req, res) => {
-    res
-      .status(200)
-      .send(
-        '<div style="display:flex; flex-direction:row; justify-content:center;width:100%; font-size:24px; overflow:none;padding:20px 0;">Api is running</div>'
-      )
-  })
 }
 
 // Middleware
@@ -34,7 +32,16 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 // Listener
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
+
+// Handle unhandled promise rejections && server will shutdown if the databsae is not connecteed
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.messasge}`.red)
+  // Close server & exit process
+  server.close(() => process.exit(1))
+})
